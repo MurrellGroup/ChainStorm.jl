f98ee2e325888fcaab98da5da41a6c16c27c68d9
murrellb <murrellb@gmail.com>
2025-05-25 00:47:39 +0200

    Aaaaand now the model

--- src/model.jl
+++ src/model.jl
@@ -33,11 +33,121 @@ function (fc::FlowcoderSC)(t, Xt, chainids, resinds; sc_frames = nothing)
     AA_one_hots = tensor(Xt[3])
     x = l.AAencoder(AA_one_hots .+ 0)
     for i in 1:l.depth
-        if sc_frames !== nothing
+        if !isnothing(sc_frames)
             x = Flux.Zygote.checkpointed(crossipa, l.selfcond_selfipa[i], sc_frames, sc_frames, x, pair_feats, cond, pmask)
             f1, f2 = mod(i, 2) == 0 ? (frames, sc_frames) : (sc_frames, frames)
             x = Flux.Zygote.checkpointed(crossipa, l.selfcond_crossipa[i], f1, f2, x, pair_feats, cond, pmask)
         end
+        x = Flux.Zygote.checkpointed(ipa, l.ipa_blocks[i], frames, x, pair_feats, cond, pmask)
+        if i > l.depth - l.f_depth
+            frames = l.framemovers[i - l.depth + l.f_depth](frames, x, t = t)
+        end
+    end
+    aa_logits = l.AAdecoder(x .+ reshape(l.AApre_t_encoding(t_rff), :, 1, size(t,2)))   
+    return frames, aa_logits
+end
+
+
+struct FlowcoderAASC{L}
+    layers::L
+end
+Flux.@layer FlowcoderAASC
+function FlowcoderAASC(dim, depth, f_depth)
+    layers = (;
+        depth = depth,
+        f_depth = f_depth,
+        t_rff = RandomFourierFeatures(1 => dim, 1f0),
+        cond_t_encoding = Dense(dim => dim, bias=false),
+        AApre_t_encoding = Dense(dim => dim, bias=false),
+        pair_rff = RandomFourierFeatures(2 => 64, 1f0),
+        pair_project = Dense(64 => 32, bias=false),
+        AAencoder = Dense(21 => dim, bias=false),
+        ipa_blocks = [IPAblock(dim, IPA(IPA_settings(dim, c_z = 32)), ln1 = AdaLN(dim, dim), ln2 = AdaLN(dim, dim)) for _ in 1:depth],
+        framemovers = [Framemover(dim) for _ in 1:f_depth],
+        AAdecoder = Chain(StarGLU(dim, 3dim), Dense(dim => 21, bias=false)),
+        #SelfCond layers:
+        selfcond_crossipa = [CrossFrameIPA(dim, IPA(IPA_settings(dim, c_z = 32)), ln = AdaLN(dim, dim)) for _ in 1:depth],
+        selfcond_selfipa = [CrossFrameIPA(dim, IPA(IPA_settings(dim, c_z = 32)), ln = AdaLN(dim, dim)) for _ in 1:depth],
+        selfcond_AAencoder = StarGLU(Dense(21 => 2dim, bias=false), Dense(2dim => dim, bias=false), Dense(21 => 2dim, bias=false), swish), #<- SwiGLU, but with different input and output dims
+    )
+    return FlowcoderAASC(layers)
+end
+function (fc::FlowcoderAASC)(t, Xt, chainids, resinds; sc_frames = nothing, sc_aa = nothing)
+    l = fc.layers
+    pmask = Flux.Zygote.@ignore self_att_padding_mask(Xt[1].lmask)
+    pre_z = Flux.Zygote.@ignore l.pair_rff(pair_encode(resinds, chainids))
+    pair_feats = l.pair_project(pre_z)
+    t_rff = Flux.Zygote.@ignore l.t_rff(t)
+    cond = reshape(l.cond_t_encoding(t_rff), :, 1, size(t,2))
+    frames = Translation(tensor(Xt[1])) ∘ Rotation(tensor(Xt[2]))
+    AA_one_hots = tensor(Xt[3])
+    x = l.AAencoder(AA_one_hots .+ 0)
+    if !isnothing(sc_frames)
+        x = x + l.selfcond_AAencoder(sc_aa)
+    end
+    for i in 1:l.depth
+        if !isnothing(sc_frames)
+            x = Flux.Zygote.checkpointed(crossipa, l.selfcond_selfipa[i], sc_frames, sc_frames, x, pair_feats, cond, pmask)
+            f1, f2 = mod(i, 2) == 0 ? (frames, sc_frames) : (sc_frames, frames)
+            x = Flux.Zygote.checkpointed(crossipa, l.selfcond_crossipa[i], f1, f2, x, pair_feats, cond, pmask)
+        end
+        x = Flux.Zygote.checkpointed(ipa, l.ipa_blocks[i], frames, x, pair_feats, cond, pmask)
+        if i > l.depth - l.f_depth
+            frames = l.framemovers[i - l.depth + l.f_depth](frames, x, t = t)
+        end
+    end
+    aa_logits = l.AAdecoder(x .+ reshape(l.AApre_t_encoding(t_rff), :, 1, size(t,2)))   
+    return frames, aa_logits
+end
+
+
+
+struct ChainStorm{L}
+    layers::L
+end
+Flux.@layer ChainStorm
+function ChainStorm(dim, depth, f_depth)
+    layers = (;
+        depth = depth,
+        f_depth = f_depth,
+        t_rff = RandomFourierFeatures(1 => dim, 1f0),
+        cond_t_encoding = Dense(dim => dim, bias=false),
+        AApre_t_encoding = Dense(dim => dim, bias=false),
+        pair_rff = RandomFourierFeatures(2 => 64, 1f0),
+        pair_project = Dense(64 => 32, bias=false),
+        AAencoder = Dense(21 => dim, bias=false),
+        ipa_blocks = [IPAblock(dim, IPA(IPA_settings(dim, c_z = 32)), ln1 = AdaLN(dim, dim), ln2 = AdaLN(dim, dim)) for _ in 1:depth],
+        framemovers = [Framemover(dim) for _ in 1:f_depth],
+        AAdecoder = Chain(StarGLU(dim, 3dim), Dense(dim => 21, bias=false)),
+        #SelfCond layers:
+        selfcond_crossipa = [CrossFrameIPA(dim, IPA(IPA_settings(dim, c_z = 32)), ln = AdaLN(dim, dim)) for _ in 1:depth],
+        selfcond_selfipa = [CrossFrameIPA(dim, IPA(IPA_settings(dim, c_z = 32)), ln = AdaLN(dim, dim)) for _ in 1:depth],
+        selfcond_AAencoder = StarGLU(Dense(21 => 2dim, bias=false), Dense(2dim => dim, bias=false), Dense(21 => 2dim, bias=false), swish), #<- SwiGLU, but with different input and output dims
+        #New Transformer Blocks:
+        transformers = [AdaTransformerBlock(dim, dim, 8) for _ in 1:depth]
+    )
+    return ChainStorm(layers)
+end
+function (fc::ChainStorm)(t, Xt, chainids, resinds; sc_frames = nothing, sc_aa = nothing)
+    l = fc.layers
+    pmask = Flux.Zygote.@ignore self_att_padding_mask(Xt[1].lmask)
+    pre_z = Flux.Zygote.@ignore l.pair_rff(pair_encode(resinds, chainids))
+    pair_feats = l.pair_project(pre_z)
+    t_rff = Flux.Zygote.@ignore l.t_rff(t)
+    cond = reshape(l.cond_t_encoding(t_rff), :, 1, size(t,2))
+    frames = Translation(tensor(Xt[1])) ∘ Rotation(tensor(Xt[2]))
+    AA_one_hots = tensor(Xt[3])
+    x = l.AAencoder(AA_one_hots .+ 0)
+    if !isnothing(sc_frames)
+        x = x + l.selfcond_AAencoder(sc_aa)
+    end
+    for i in 1:l.depth
+        if !isnothing(sc_frames)
+            x = Flux.Zygote.checkpointed(crossipa, l.selfcond_selfipa[i], sc_frames, sc_frames, x, pair_feats, cond, pmask)
+            f1, f2 = mod(i, 2) == 0 ? (frames, sc_frames) : (sc_frames, frames)
+            x = Flux.Zygote.checkpointed(crossipa, l.selfcond_crossipa[i], f1, f2, x, pair_feats, cond, pmask)
+        end
+        x = Flux.Zygote.checkpointed(l.transformers[i], x, cond, nothing, pmask)
         x = Flux.Zygote.checkpointed(ipa, l.ipa_blocks[i], frames, x, pair_feats, cond, pmask)
         if i > l.depth - l.f_depth
             frames = l.framemovers[i - l.depth + l.f_depth](frames, x, t = t)
